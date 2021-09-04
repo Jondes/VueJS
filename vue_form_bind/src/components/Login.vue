3d778552c8b4d43f9415e406c8515d165b0093ce
Johnson E Guedes <jondes560@hotmail.com>
2021-09-04 18:20:08 +0100

    Initial Commit
    form binding in vue.js

---
+++ vue_form_bind/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <input type="text" v-model="loginForm.username" placeholder="enter username" /><br><br>
+    <input type="text" v-model="loginForm.password" placeholder="enter password" /><br><br>
+    <button v-on:click="getLogin">Login</button>
+</div>
+</template>
+
+<script>
+export default{
+        name: 'Login',
+        data(){
+            return {
+                loginForm: {
+                username:null,
+                password:null
+            }
+        }
+     
+    },
+    methods:{
+        getLogin(){
+            console.log("getLogin called",this.loginForm.username,this.loginForm.password)
+        }
+    }
+}
+</script>
