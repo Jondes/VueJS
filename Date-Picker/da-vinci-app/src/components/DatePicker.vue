f31521f109009fa0c3d40ed40c29fcb2351e19cb
Johnson E Guedes <jondes560@hotmail.com>
2021-09-06 13:35:16 +0100

    Initial Commit
    Vue.js Date-picker

---
+++ Date-Picker/da-vinci-app/src/components/DatePicker.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Vue Simple Datepicker Example</h2>
+        <Datepicker></Datepicker>
+    </div>
+</template>
+
+<script>
+    import Datepicker from 'vuejs-datepicker'
+
+    export default {
+        name: 'app',
+        components: {
+            Datepicker
+        }
+    }
+</script>
