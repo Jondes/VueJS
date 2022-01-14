64fa613f130ffc30c61750a7745bcbbf2251ec53
Johnson E Guedes <jondes560@hotmail.com>
2022-01-14 16:31:38 +0000

    nuxt.js
    nuxt.js

---
+++ vue-date-picker/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <CalendarWidget />
+    
+  </div>
+</template>
+
+<script>
+  import CalendarWidget from './components/CalendarWidget.vue'
+  
+
+
+  export default {
+    name: 'App',
+    components: {
+      CalendarWidget
+    }
+  }
+</script>
+
+<style scoped>
+ .container {
+     max-width: 500px;
+ }
+</style>
