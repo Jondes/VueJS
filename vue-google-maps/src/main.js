8e725df129f014bd939cdce83fcf08c8934c874c
Johnson E Guedes <jondes560@hotmail.com>
2022-01-17 07:51:39 +0000

    google maps
    google maps initial

---
+++ vue-google-maps/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import * as VueGoogleMaps from "vue2-google-maps" // Import package
+
+Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyAzbGsehQifpsHDNW_8a3E5MTwikCeQgM0",
+    libraries: "places"
+  }
+});
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
