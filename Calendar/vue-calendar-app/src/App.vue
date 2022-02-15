5a0db9a6c7e0ab17132a3e062108be22eaceb5c3
Johnson E Guedes <jondes560@hotmail.com>
2022-02-15 21:34:36 +0000

    Vue calendar app
    Vue.js calendar

---
+++ Calendar/vue-calendar-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <CalendarWidget />
+  </div>
+</template>
+<script>
+  import CalendarWidget from './components/CalendarWidget.vue'
+  export default {
+    name: 'App',
+    components: {
+      CalendarWidget
+    }
+  }
+</script>
+<style scoped>
+ .container {
+     max-width: 500px;
+ }
+</style>
