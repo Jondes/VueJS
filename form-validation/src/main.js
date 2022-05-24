166e478ba4e810851af8a1a4b8944ac8e3cc6b74
Johnson E Guedes <jondes560@hotmail.com>
2022-05-24 21:39:00 +0100

    Vue Form validation
    Vue form

---
+++ form-validation/src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate';
+import * as rules from 'vee-validate/dist/vee-validate';
+import App from "./App.vue";
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+// install rules
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+// Install components globally
+Vue.use(BootstrapVue);
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.config.productionTip = false;
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
